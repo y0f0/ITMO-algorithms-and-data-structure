7c0ca995ec593e63c20ecdc80f75bc2859d4ed8b
Nikita <31010564+y0f0@users.noreply.github.com>
2020-10-10 11:13:42 +0300

    labs3

---
+++ Labs3/a.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <vector>
+
+bool isHeap(const std::vector<int>& a, const int& n) {
+    for (int i = 1; i <= n; i++) {
+        if (2 * i <= n) {
+            if (a[i] > a[2 * i]) {
+                return false;
+            }
+        }
+        if (2 * i + 1  <= n) {
+            if (a[i] > a[2 * i + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::ifstream fin("isheap.in");
+    std::ofstream fout("isheap.out");
+
+    int n;
+
+    fin >> n;
+
+    std::vector<int> a(n + 1); 
+
+    for (int i = 1; i <= n; i++) {
+
+        fin >> a[i];
+
+    }
+
+    isHeap(a, n) ? fout << "YES\n" : fout << "NO\n";
+
+    fin.close();
+    fout.close();
+    return 0;
+}
---
+++ Labs3/b.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+void MaxHeapify(std::vector<int>& a, int heap_size, int i) {
+
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+
+    if (l < heap_size && a[l] > a[largest]) {
+        largest = l;
+    }
+
+    if (r < heap_size && a[r] > a[largest]) {
+        largest = r;
+    }
+
+    if (largest != i) {
+
+        std::swap(a[i], a[largest]);
+        MaxHeapify(a, heap_size, largest);
+
+    }
+}
+
+void HeapSort(std::vector<int>& a) {
+
+    int heap_size = a.size();
+
+    for (int i = heap_size / 2 - 1; i >= 0; i--) {
+        MaxHeapify(a, heap_size, i);
+    }
+
+    for (int i = heap_size - 1; i > 0; i--) {
+        std::swap(a[0], a[i]);
+
+        MaxHeapify(a, i, 0);
+    }
+}
+
+
+int main() {
+
+    std::ifstream fin("sort.in");
+    std::ofstream fout("sort.out");
+
+    int n;
+    
+    fin >> n;
+
+    std::vector<int> v(n); 
+
+    for (auto& element : v) {
+
+        fin >> element;
+
+    } 
+
+    HeapSort(v);
+
+    for (auto& e : v) { 
+
+        fout << e << ' ';
+
+    }
+    
+    fin.close();
+    fout.close();
+
+    return 0;
+}
